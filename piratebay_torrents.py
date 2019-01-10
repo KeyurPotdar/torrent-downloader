068910fd3bff8585e03468277a9bb4dee2891e57
Keyur Potdar <keyurpotdar@gmail.com>
2019-01-10 19:36:50 +0530

    Update piratebay_torrents.py

--- piratebay_torrents.py
+++ piratebay_torrents.py
@@ -8,7 +8,7 @@ import tkinter as tk
 from bs4 import BeautifulSoup
 from multiprocessing import Process
 
-WEBSITE_NAME = 'https://piratepirate.eu'
+WEBSITE_NAME = 'https://bayhypertpb.be'
 WEBSITE_PREFIX = '/s/?q='
 WEBSITE_SPACE = '+'
 WEBSITE_SUFFIX = '&page=0&orderby=99'
@@ -21,15 +21,27 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleW
 
 
 def format_episode(season, episode):
+    """
+    Returns the complete episode formed using `season` and `episode`.
+
+    Example:
+    >>> format_episode(3, 11)
+    's03e11'
+    """
     return 's{:02d}e{:02d}'.format(season, episode)
 
 
 def format_url(name, season=None, episode=None):
     """
-    Usage:
-    (1): format_url('the office', 4, 11)
-    (2): format_url('the office', 's04e11')
-    (3): format_url('the office s04e11')
+    Returns the URL formed by using `name`, `season` and `episode`.
+
+    Example:
+    >>> format_url('the office', 4, 11)
+    'https://bayhypertpb.be/s/?q=the+office+s04e11&page=0&orderby=99'
+    >>> format_url('the office', 's04e11')
+    'https://bayhypertpb.be/s/?q=the+office+s04e11&page=0&orderby=99'
+    >>> format_url('the office s04e11')
+    'https://bayhypertpb.be/s/?q=the+office+s04e11&page=0&orderby=99'
     """
     if season is None and episode is None:
         return WEBSITE_NAME + WEBSITE_PREFIX + name.replace(' ', WEBSITE_SPACE) + WEBSITE_SUFFIX
@@ -41,6 +53,9 @@ def format_url(name, season=None, episode=None):
 
 
 def download_torrent(link, root=None):
+    """
+    Starts the torrent download. Requires any torrent application pre-installed (μTorrent, BitTorrent, etc).
+    """
     try:
         if root:
             root.destroy()
@@ -48,12 +63,16 @@ def download_torrent(link, root=None):
         soup = BeautifulSoup(r.text, 'lxml')
         magnet = soup.find('a', {'title': 'Get this torrent'}).get('href')
 
+        # Linux
         if sys.platform.startswith('linux'):
             subprocess.Popen(['xdg-open', magnet], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Windows
         elif sys.platform.startswith('win32'):
             os.startfile(magnet)
+        # Cygwin
         elif sys.platform.startswith('cygwin'):
             os.startfile(magnet)
+        # macOS
         elif sys.platform.startswith('darwin'):
             subprocess.Popen(['open', magnet], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
@@ -62,7 +81,11 @@ def download_torrent(link, root=None):
         logging.error(str(e), exc_info=True)
 
 
-def show_torrent_links(url):
+def show_torrents(url):
+    """
+    Scrapes the PirateBay proxy and displays all available torrents.
+    """
+    # Establish connection and get page content
     try:
         r = requests.get(url, headers=headers)
         r.raise_for_status()
@@ -80,6 +103,7 @@ def show_torrent_links(url):
     for col, label in enumerate(['Sr No', 'NAME', 'SE', 'LE', 'SIZE', 'UPLOADED', 'LINK'], 1):
         tk.Label(root, text=label).grid(column=col, row=1, sticky=tk.W, padx=10, pady=10)
 
+    # Scrape the page
     soup = BeautifulSoup(r.text, 'lxml')
     for row, tr in enumerate(soup.select('#searchResult tr')[1:15], 1):
         det_link = tr.find('a', {'class': 'detLink'})
@@ -128,17 +152,17 @@ def main():
             for x in e:
                 x = x.strip()
                 if re.match(r'\d+-\d+', x):
-                    f, t = map(int, re.search('(\d+)-(\d+)', x).groups())
+                    f, t = map(int, re.search(r'(\d+)-(\d+)', x).groups())
                     for ep in range(f, t+1):
                         try:
                             url = format_url(n, s, ep)
-                            Process(target=show_torrent_links, args=(url,)).start()
+                            Process(target=show_torrents, args=(url,)).start()
                         except Exception as e:
                             logging.error(str(e), exc_info=True)
                 else:
                     try:
                         url = format_url(n, s, int(x))
-                        Process(target=show_torrent_links, args=(url,)).start()
+                        Process(target=show_torrents, args=(url,)).start()
                     except ValueError:
                         continue
                     except Exception as e:
@@ -160,7 +184,7 @@ def main():
     def button2_click(event=None):
         s = search.get().strip()
         if s:
-            show_torrent_links(format_url(s))
+            show_torrents(format_url(s))
 
     search.bind('<Return>', button2_click)
     tk.Button(root, text='Get Torrents', command=button2_click).grid(row=6, column=1, padx=10, pady=(4, 10), sticky='w')
